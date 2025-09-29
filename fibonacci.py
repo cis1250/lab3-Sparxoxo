f457b10f1f6c3334fabd79afdc6af241ca20feed
Amanda Parco <amandaparco@gmail.com>
2025-09-29 12:08:13 -0400

    Correct Fibonacci sequence input and output formatting
    Fixed indentation and placement of print statements in the Fibonacci sequence generator.

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,12 @@
 while True:
-num = input("How many terms of the Fibonacci sequence do you want? ")
-if num.isdigit() and int(num) > 0:  
-n = int(num)
-break
-else:
-print("Please enter a positive integer.")
+    num = input("How many terms of the Fibonacci sequence do you want? ")
+    if num.isdigit() and int(num) > 0:  
+        n = int(num)
+        break
+    else:
+        print("Please enter a positive integer.")
+
 a, b = 0, 1
 for i in range(n):
-print(a, end=" ")
-a, b = b, a + b
+    print(a, end=" ")
+    a, b = b, a + b  # This must be inside the loop
