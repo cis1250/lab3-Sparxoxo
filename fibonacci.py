a5dcf3405161dad2c405eb95fdebc7df99dc8495
Amanda Parco <amandaparco@gmail.com>
2025-09-29 12:00:08 -0400

    Clean up comments in fibonacci.py
    Removed introductory comments and instructions.

--- fibonacci.py
+++ fibonacci.py
@@ -1,15 +1,6 @@
-#!/usr/bin/env python3
-
-# Fibonacci Sequence Exercise
-# TODO: (Read detailed instructions in the Readme file)
-# Prompt the user for the number of terms.
-# Validate that the input is a positive integer.
-# Use a for loop to print the Fibonacci sequence up to that many terms.
-# fibonacci.py
-
 while True:
     num = input("How many terms of the Fibonacci sequence do you want? ")
-    if num.isdigit() and int(num) > 0:   # check if it's a positive integer
+    if num.isdigit() and int(num) > 0:  
         n = int(num)
         break
     else:
