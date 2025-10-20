211dcaa2cadb4b027d31ddbe0170d554bab54d17
Amanda Parco <amandaparco@gmail.com>
2025-10-20 12:17:45 -0400

    Refactor Fibonacci sequence code into functions

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,15 @@
-while True:
-    num = input("How many terms of the Fibonacci sequence do you want? ")
-    if num.isdigit() and int(num) > 0:  
-        n = int(num)
-        break
-    else:
+def get_positive_integer(prompt):
+    while True:
+        num = input(prompt)
+        if num.isdigit() and int(num) > 0:
+            return int(num)
         print("Please enter a positive integer.")
 
-a, b = 0, 1
-for i in range(n):
-    print(a, end=" ")
-    a, b = b, a + b
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+
+n = get_positive_integer("How many terms of the Fibonacci sequence do you want? ")
+fibonacci(n)
